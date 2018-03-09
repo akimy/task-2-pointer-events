4a1f3c05d54319963bfb7c96003c1e84b0c93a65
Alexander Vaganov <ar.vaganov@gmail.com>
2018-03-09 12:27:25 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+/**
+ * @class App
+ * @param {Element} el
+ */
+function App(el) {
+  const appEl = el;
+  const doors = [
+    new Door0(0, onUnlock),
+    new Door1(1, onUnlock),
+    new Door2(2, onUnlock),
+    new Box(3, onUnlock),
+  ];
+
+  this.doors = doors;
+
+  /**
+     * Callback вызывается в коде двери
+     * Тут даем возможность открыть следующие двери
+     */
+  function onUnlock() {
+    let previousUnlocked;
+
+    // Даем открыть следующую дверь
+    for (let i = 0; i < doors.length; i++) {
+      if (!doors[i].isLocked) {
+        previousUnlocked = true;
+      } else if (previousUnlocked && doors[i].isLocked) {
+        doors[i].enable();
+        break;
+      }
+    }
+  }
+}
+
+// Start the app
+const app = new App(document.querySelector('.app'));
