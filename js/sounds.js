099c6041af079983a5762f6f874d03cbb822b20d
Alexander Vaganov <ar.vaganov@gmail.com>
2018-03-10 21:01:22 +0300

    remove ost-theme / change event handling for ios

--- js/sounds.js
+++ js/sounds.js
@@ -22,13 +22,14 @@ class Sounds {
 const sounds = new Sounds();
 
 document.addEventListener('DOMContentLoaded', () => {
-  Array.prototype.forEach.call(document.querySelectorAll('.brief__template'), (el) => {
-    el.addEventListener('pointerdown', (e) => {
-      e.target.remove();
+  const briefs = document.querySelectorAll('.brief__template');
+  briefs.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      const { target } = event;
+      target.style.display = 'none';
     });
   });
 
-  document.querySelector('.ost__main-theme').volume = '0.4';
   sounds.setTrack(document.querySelector('.ost__click'), 'Click');
   sounds.setTrack(document.querySelector('.ost__shutter'), 'Shutter');
   sounds.setTrack(document.querySelector('.ost__door-opened'), 'DoorOpened');
